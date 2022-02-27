fcfc3267909cbf585b0b71e0059698b156946dac
boring-plans <boring-plans@users.noreply.github.com>
2022-02-27 07:47:06 +0000

    deploy: 6f42447a844a0d2a9c57dd173dd42cb5c789162c

--- _nuxt/static/1645947117/Stereotyped/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/Stereotyped", {data:[{articles:[{title:"2021 年 5 月前端造火箭副本掉落",date:"2021-05-18",to:"\u002FStereotyped\u002F2021-05-18_2021 年 5 月前端造火箭副本掉落"},{title:"2021 年 4 月前端造火箭副本掉落",date:"2021-04-26",to:"\u002FStereotyped\u002F2021-04-26_2021 年 4 月前端造火箭副本掉落"}]}],fetch:{},mutations:[]});
