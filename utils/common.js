8c4c4cd3c3f484bdff8dd89c305dfa99e39fa397
kang1.tao <kang1.tao@tcl.com>
2022-05-05 15:28:53 +0800

    fix: fixes #6

--- utils/common.js
+++ utils/common.js
@@ -2,7 +2,7 @@ import fireworks from 'fireworks/lib/fireworks'
 
 export const star = (ev) => {
   const div = document.createElement('div')
-  div.style = 'position: fixed; width: 100vw; height:100vh; top: 0; left: 0'
+  div.style = 'position: fixed; width: 100vw; height:100vh; top: 0; left: 0; z-index: 999;'
   document.body.appendChild(div)
   fireworks({
     x: ev.x,
